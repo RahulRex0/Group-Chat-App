6bba7c94a2484f86781861363b8f8f28cad950e5
RahulRex0 <75500427+RahulRex0@users.noreply.github.com>
2026-07-13 19:12:09 +0530

    Integrates authenticated API calls for channels

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,8 +2,15 @@
 
 import { redirect } from 'next/navigation'
 import { revalidatePath } from 'next/cache'
+import { cookies } from 'next/headers'
+
+const api = process.env.NEXT_PUBLIC_API_URL
 
 export async function signOut() {
+  
+  const cookieStore = await cookies()
+  cookieStore.delete('token')
+
   redirect('/login')
 }
 
@@ -11,6 +18,18 @@ export async function createChannel(formData: FormData) {
   const name = formData.get('name') as string
   if (!name?.trim()) return
 
+    const token = (await cookies()).get("token")?.value
+    if(!token){
+      redirect("/login")
+    }
+
+    const res= await fetch(`${api}/channels`,{
+      method:"post",
+      headers:{"content-type":"application/json",Cookie: `token=${token}`},
+      body:JSON.stringify({ name })
+    })
+
+  if(!res.ok){return}
   revalidatePath('/')
 }
 
@@ -26,5 +45,17 @@ export async function deleteChannel(formData: FormData) {
   const channelId = formData.get('channelId') as string
   if (!channelId) return
 
+  const token = (await cookies()).get("token")?.value
+  if(!token){
+    redirect("/login")
+  }
+
+  const res= await fetch(`${api}/channels/${channelId}`,{
+    method:"delete",
+    headers:{Cookie: `token=${token}`}
+  })
+
+  if(!res.ok){return}
+  
   revalidatePath('/')
 }
